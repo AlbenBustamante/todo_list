2d18184a5e48215a1b6dedf393d6fa565dd605ac
Alben <albensnico@gmail.com>
2022-05-06 14:15:00 -0500

    Add path to task controller

--- src/main/java/com/alnicode/todolist/web/controller/TaskController.java
+++ src/main/java/com/alnicode/todolist/web/controller/TaskController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/tasks")
 @Validated
 public class TaskController extends CrudController<TaskRequest, TaskResponse> {
     @Autowired
