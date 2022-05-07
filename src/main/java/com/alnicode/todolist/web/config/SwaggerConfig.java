8bda09dea1b5ea9880b56e58c049b98931fb1341
Alben <albensnico@gmail.com>
2022-05-07 15:36:20 -0500

    Remove unnecessary import

--- src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
+++ src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
@@ -1,14 +1,14 @@
 package com.alnicode.todolist.web.config;
 
+import static com.alnicode.todolist.util.AppConstants.BASE_PACKAGE;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.alnicode.todolist.util.AppConstants.BASE_PACKAGE;
 
 @Configuration
 @EnableSwagger2
