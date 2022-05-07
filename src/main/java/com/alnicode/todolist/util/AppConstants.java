90bffe6129926d24f4211d3a7e836f3915b2cea5
Alben <albensnico@gmail.com>
2022-05-07 16:26:35 -0500

    Add project info and constants

--- src/main/java/com/alnicode/todolist/util/AppConstants.java
+++ src/main/java/com/alnicode/todolist/util/AppConstants.java
@@ -3,4 +3,17 @@ package com.alnicode.todolist.util;
 public interface AppConstants {
     String DATE_FORMAT = "dd-MM-yyyy";
     String BASE_PACKAGE = "com.alnicode.todolist.web.controller";
+    String APP_TITLE = "To-Do List";
+    String APP_DESCRIPTION = "As the title says, it's a to-do list, no more, no less.\n" +
+        "How does it work? Simple, you create a very general category of something that you have pending.\n" +
+        "You create a new list with the name you want and assign that category.\n" +
+        "Now, create the tasks that you have pending and assign them the list.\n" +
+        "Also, you can be more specific by creating and assigning labels to it.\n" +
+        "At the end, when you finish your task, update the status to true.\n" +
+        "You can see the source code on my GitHub :D";
+    String APP_LICENSE = "Apache License, Version 2.0";
+    String APP_LICENSE_URL = "https://www.apache.org/licenses/LICENSE-2.0";
+    String CONTACT_NAME = "alnicode";
+    String CONTACT_URL = "https://www.linkedin.com/in/alben-bustamante/";
+    String CONTACT_EMAIL = "albenbustamante@gmail.com";
 }
--- src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
+++ src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
@@ -1,11 +1,21 @@
 package com.alnicode.todolist.web.config;
 
+import static com.alnicode.todolist.util.AppConstants.APP_TITLE;
+import static com.alnicode.todolist.util.AppConstants.APP_DESCRIPTION;
+import static com.alnicode.todolist.util.AppConstants.APP_LICENSE;
+import static com.alnicode.todolist.util.AppConstants.APP_LICENSE_URL;
 import static com.alnicode.todolist.util.AppConstants.BASE_PACKAGE;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_EMAIL;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_NAME;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_URL;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +28,25 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .build()
+                .apiInfo(this.info());
+    }
+
+    private ApiInfo info() {
+        return new ApiInfoBuilder()
+                .contact(this.contact())
+                .title(APP_TITLE)
+                .description(APP_DESCRIPTION)
+                .license(APP_LICENSE)
+                .licenseUrl(APP_LICENSE_URL)
                 .build();
     }
     
+    private Contact contact() {
+        return new Contact(
+                CONTACT_NAME,
+                CONTACT_URL,
+                CONTACT_EMAIL);
+    }
+
 }
