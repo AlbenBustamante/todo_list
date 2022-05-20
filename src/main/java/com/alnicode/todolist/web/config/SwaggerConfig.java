8168ead4af86bc1352098c0e81216e2a455ce037
Alben Bustamante <albenbustamante@gmail.com>
2022-05-20 17:38:45 -0500

    Add security scheme

--- src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
+++ src/main/java/com/alnicode/todolist/web/config/SwaggerConfig.java
@@ -1,24 +1,26 @@
 package com.alnicode.todolist.web.config;
 
-import static com.alnicode.todolist.util.AppConstants.APP_TITLE;
-import static com.alnicode.todolist.util.AppConstants.APP_DESCRIPTION;
-import static com.alnicode.todolist.util.AppConstants.APP_LICENSE;
-import static com.alnicode.todolist.util.AppConstants.APP_LICENSE_URL;
-import static com.alnicode.todolist.util.AppConstants.BASE_PACKAGE;
-import static com.alnicode.todolist.util.AppConstants.CONTACT_EMAIL;
-import static com.alnicode.todolist.util.AppConstants.CONTACT_NAME;
-import static com.alnicode.todolist.util.AppConstants.CONTACT_URL;
-
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import static com.alnicode.todolist.util.AppConstants.APP_DESCRIPTION;
+import static com.alnicode.todolist.util.AppConstants.APP_LICENSE;
+import static com.alnicode.todolist.util.AppConstants.APP_LICENSE_URL;
+import static com.alnicode.todolist.util.AppConstants.APP_TITLE;
+import static com.alnicode.todolist.util.AppConstants.BASE_PACKAGE;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_EMAIL;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_NAME;
+import static com.alnicode.todolist.util.AppConstants.CONTACT_URL;
 
 @Configuration
 @EnableSwagger2
@@ -26,10 +28,15 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
                 .build()
                 .apiInfo(this.info());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT Token", "Authorization", "header");
     }
 
     private ApiInfo info() {
