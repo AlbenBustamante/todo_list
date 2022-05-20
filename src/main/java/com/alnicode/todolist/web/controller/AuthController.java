4cec7288f92fe23f57ad746506455600b34e45fe
Alben Bustamante <albenbustamante@gmail.com>
2022-05-20 16:44:39 -0500

    Add RequestBody annotation

--- src/main/java/com/alnicode/todolist/web/controller/AuthController.java
+++ src/main/java/com/alnicode/todolist/web/controller/AuthController.java
@@ -10,13 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Validated
 public class AuthController {
     @Autowired
     private AuthenticationManager manager;
@@ -28,7 +32,7 @@ public class AuthController {
     private JwtUtil jwtUtil;
 
     @PostMapping
-    public ResponseEntity<AuthenticationResponse> getToken(@NotNull @Valid AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> getToken(@NotNull @Valid @RequestBody AuthenticationRequest request) {
         try {
             manager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
             final var userDetails = userDetailsService.loadUserByUsername(request.getUsername());
@@ -37,7 +41,7 @@ public class AuthController {
             response.setJwt(jwt);
 
             return ResponseEntity.ok(response);
-        } catch (Exception ex) {
+        } catch (BadCredentialsException ex) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
     }
