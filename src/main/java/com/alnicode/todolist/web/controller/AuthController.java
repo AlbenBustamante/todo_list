de21333c0af6cc4eb1955e71553bcb2561712ff4
Alben Bustamante <albenbustamante@gmail.com>
2022-05-20 14:37:43 -0500

    Create authentication controller

---
+++ src/main/java/com/alnicode/todolist/web/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.alnicode.todolist.web.controller;
+
+import com.alnicode.todolist.domain.dto.AuthenticationRequest;
+import com.alnicode.todolist.domain.dto.AuthenticationResponse;
+import com.alnicode.todolist.domain.service.impl.CustomUserDetailsService;
+import com.alnicode.todolist.web.security.JwtUtil;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<AuthenticationResponse> getToken(@NotNull @Valid AuthenticationRequest request) {
+        try {
+            manager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+            final var userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+            final var jwt = jwtUtil.generateToken(userDetails);
+            final var response = new AuthenticationResponse();
+            response.setJwt(jwt);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+}
