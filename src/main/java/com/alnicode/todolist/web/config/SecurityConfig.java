6c184c255e807f914d1b3ada6ee6070a6b0d25cf
Alben Bustamante <albenbustamante@gmail.com>
2022-05-20 17:12:43 -0500

    Ignore swagger to configuration

--- src/main/java/com/alnicode/todolist/web/config/SecurityConfig.java
+++ src/main/java/com/alnicode/todolist/web/config/SecurityConfig.java
@@ -2,18 +2,19 @@ package com.alnicode.todolist.web.config;
 
 import com.alnicode.todolist.domain.service.impl.CustomUserDetailsService;
 import com.alnicode.todolist.web.security.filter.JwtFilterRequest;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -53,5 +54,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/webjars/**"
+        );
     }
 }
