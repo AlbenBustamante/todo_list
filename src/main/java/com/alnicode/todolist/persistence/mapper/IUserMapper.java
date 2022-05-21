64dee5a26918620118fff0f7790d8bd654de3f4e
Alben Bustamante <albenbustamante@gmail.com>
2022-05-21 00:45:13 -0500

    Remove unnecessary mapping annotation

--- src/main/java/com/alnicode/todolist/persistence/mapper/IUserMapper.java
+++ src/main/java/com/alnicode/todolist/persistence/mapper/IUserMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IUserMapper {
-    @Mapping(target = "role", ignore = true)
     UserResponse toResponse(User user);
 
     @Mapping(target = "id", ignore = true)
