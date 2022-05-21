34aa9c9cabb3ca0560e77fcdfd263d271d65d4ed
Alben Bustamante <albenbustamante@gmail.com>
2022-05-21 00:42:59 -0500

    Create method to set a default role to the new users

--- src/main/java/com/alnicode/todolist/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/alnicode/todolist/domain/service/impl/UserServiceImpl.java
@@ -3,8 +3,13 @@ package com.alnicode.todolist.domain.service.impl;
 import com.alnicode.todolist.domain.dto.AuthenticationRequest;
 import com.alnicode.todolist.domain.dto.UserResponse;
 import com.alnicode.todolist.domain.service.IUserService;
+import com.alnicode.todolist.persistence.entity.Role;
+import com.alnicode.todolist.persistence.entity.User;
 import com.alnicode.todolist.persistence.mapper.IUserMapper;
+import com.alnicode.todolist.persistence.repository.IRoleRepository;
 import com.alnicode.todolist.persistence.repository.IUserRepository;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,11 +25,30 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private IRoleRepository roleRepository;
+
     @Override
     public UserResponse register(AuthenticationRequest request) {
         final var entity = mapper.toEntity(request);
         entity.setPassword(encoder.encode(request.getPassword()));
 
         return mapper.toResponse(repository.save(entity));
+    }
+
+    private void setRole(User entity) {
+        final var role = roleRepository.findByName("USER");
+        final Set<Role> roles = new HashSet<>();
+
+        if (role.isPresent()) {
+            roles.add(role.get());
+        } else {
+            final var newRole = new Role();
+            newRole.setName("USER");
+
+            roles.add(newRole);
+        }
+
+        entity.setRoles(roles);
     }
 }
