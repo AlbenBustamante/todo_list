15238669388b63a4c531b19aafad75d435abec5f
Alben Bustamante <albenbustamante@gmail.com>
2022-05-21 00:11:07 -0500

    Create user entity/dto mapper

---
+++ src/main/java/com/alnicode/todolist/persistence/mapper/IUserMapper.java
@@ -0,0 +1,17 @@
+package com.alnicode.todolist.persistence.mapper;
+
+import com.alnicode.todolist.domain.dto.AuthenticationRequest;
+import com.alnicode.todolist.domain.dto.UserResponse;
+import com.alnicode.todolist.persistence.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface IUserMapper {
+    @Mapping(target = "role", ignore = true)
+    UserResponse toResponse(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    User toEntity(AuthenticationRequest request);
+}
