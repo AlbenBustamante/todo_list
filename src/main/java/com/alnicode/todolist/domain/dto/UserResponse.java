5395eb1039a81c59acb4a15eb42a54b13b2cd549
Alben Bustamante <albenbustamante@gmail.com>
2022-05-21 00:44:13 -0500

    Replace String to collection

--- src/main/java/com/alnicode/todolist/domain/dto/UserResponse.java
+++ src/main/java/com/alnicode/todolist/domain/dto/UserResponse.java
@@ -1,10 +1,12 @@
 package com.alnicode.todolist.domain.dto;
 
+import com.alnicode.todolist.persistence.entity.Role;
+import java.util.Set;
 import lombok.Data;
 
 @Data
 public class UserResponse {
     private long id;
     private String username;
-    private String role;
+    private Set<Role> roles;
 }
