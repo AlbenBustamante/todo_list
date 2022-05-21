f9befe5f6a98de6ed54e539bba52956d6fc1a7e2
Alben Bustamante <albenbustamante@gmail.com>
2022-05-21 00:26:29 -0500

    Add method to repository

--- src/main/java/com/alnicode/todolist/persistence/repository/IRoleRepository.java
+++ src/main/java/com/alnicode/todolist/persistence/repository/IRoleRepository.java
@@ -1,9 +1,11 @@
 package com.alnicode.todolist.persistence.repository;
 
 import com.alnicode.todolist.persistence.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
